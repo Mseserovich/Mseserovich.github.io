480a6886c8a21d22095c870d6b9032967e58c3ee
Tshepo Tshabalala <tshepo.tshabalala@moov.life>
2021-03-09 08:03:23 +0200

    added content to the site

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
 })
 
+//Reset translateX to 0 
 navLinks.forEach(linkItem => {
     linkItem.addEventListener('click', () => {
         document.body.classList.remove('nav-open');
