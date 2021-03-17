242ba853650ab2ba2f0e4e5a5e8a61df069b9b06
Tshepo Tshabalala <tshepo.tshabalala@moov.life>
2021-03-17 08:10:49 +0200

    changes

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 let navToggle = document.querySelector('.nav-toggle');
 let navLinks = document.querySelectorAll('.nav__link');
-let y = document.querySelector('.portfolio__item1');
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
@@ -12,17 +11,3 @@ navLinks.forEach(linkItem => {
         document.body.classList.remove('nav-open');
     })
 })
-
-y.addEventListener('click', () => {
-    myFunction();
-    
-})
-
-function myFunction() {
-    var x = document.querySelector('.modal')
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
