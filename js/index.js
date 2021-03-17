67c9c69ba18826d918c55512e5206df3704d0958
Tshepo Tshabalala <tshepo.tshabalala@moov.life>
2021-03-17 06:30:47 +0200

    added cv and changed pic

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 let navToggle = document.querySelector('.nav-toggle');
 let navLinks = document.querySelectorAll('.nav__link');
+let y = document.querySelector('.portfolio__item1');
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
@@ -11,3 +12,17 @@ navLinks.forEach(linkItem => {
         document.body.classList.remove('nav-open');
     })
 })
+
+y.addEventListener('click', () => {
+    myFunction();
+    
+})
+
+function myFunction() {
+    var x = document.querySelector('.modal')
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
